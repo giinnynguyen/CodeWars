b70aae9ad6725409f2cfa0182b4133f92d6a2ab6
giinnynguyen <tieuphung2205@gmail.com>
2022-09-21 19:01:33 +0800

    Update StringCleaning.py

--- Regular Expression/8kyu/StringCleaning.py
+++ Regular Expression/8kyu/StringCleaning.py
@@ -1,8 +1,8 @@
 # https://www.codewars.com/kata/57e1e61ba396b3727c000251
 import re
 
-#(): Match a group of characters and remembering the match
 # d+: match one or more digit
+import re
 def string_clean(s):
-    s = re.sub("(\d)+", "", s)
+    s = re.sub("\d+", "", s)
     return s
