2ce749c12338e2e00a1b61eb6cb9545ce85ede5b
giinnynguyen <tieuphung2205@gmail.com>
2022-10-06 19:50:36 +0800

    Update and rename AStrangeTripToMarket.md to AStrangeTripToMarket.java

---
+++ Regular Expression/8kyu/AStrangeTripToMarket.java
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/55ccdf1512938ce3ac000056 <br>
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+public class Nessie {
+    public static boolean isLockNessMonster(String s){
+       Pattern p = Pattern.compile("tree fiddy|3.50");
+       Matcher m = p.matcher(s);
+       return m.find();
+    }
+}
