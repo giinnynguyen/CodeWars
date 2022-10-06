698e8133c807073bd6172177a6f8743a5da6ea68
giinnynguyen <tieuphung2205@gmail.com>
2022-10-06 19:53:18 +0800

    Update AStrangeTripToMarket.java

--- Regular Expression/8kyu/AStrangeTripToMarket.java
+++ Regular Expression/8kyu/AStrangeTripToMarket.java
@@ -1,4 +1,5 @@
 // https://www.codewars.com/kata/55ccdf1512938ce3ac000056 <br>
+// check if string contain tree fiddy or 3.50
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 public class Nessie {
