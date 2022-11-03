fe3f668f17938aa0f874d7df1ce97ff5d2cd510d
giinnynguyen <tieuphung2205@gmail.com>
2022-11-03 14:42:04 +0800

    Update DisemvowelTrolls.java

--- Regular Expression/7kyu/DisemvowelTrolls.java
+++ Regular Expression/7kyu/DisemvowelTrolls.java
@@ -1,4 +1,5 @@
 // https://www.codewars.com/kata/52fba66badcd10859f00097e
+//remove all vowel from a string
 public class DisemvowelTrolls {
     public static String disemvowel(String str) {
        return str.replaceAll("u|U|e|E|o|O|a|A|i|I","");
