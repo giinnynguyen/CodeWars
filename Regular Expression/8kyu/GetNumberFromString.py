218b6377721bfa5597eda32a1af5cc167106b6c1
giinnynguyen <tieuphung2205@gmail.com>
2022-11-03 17:51:48 +0800

    Update GetNumberFromString.py

--- Regular Expression/8kyu/GetNumberFromString.py
+++ Regular Expression/8kyu/GetNumberFromString.py
@@ -1,5 +1,5 @@
 # https://www.codewars.com/kata/57a37f3cbb99449513000cd8
-# Write a function which removes from string all non-digit characters and parse the remaining to number. E.g: "hell5o wor6ld" -> 56
+# removes from string all non-digit characters and parse the remaining to number. E.g: "hell5o wor6ld" -> 56
 
 import re
 def get_number_from_string(string):
