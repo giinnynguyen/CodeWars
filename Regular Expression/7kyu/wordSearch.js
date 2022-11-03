d3924df5008d1851723dd4bd07d8da86e8e78b9c
giinnynguyen <tieuphung2205@gmail.com>
2022-11-03 17:50:51 +0800

    Update wordSearch.js

--- Regular Expression/7kyu/wordSearch.js
+++ Regular Expression/7kyu/wordSearch.js
@@ -1,4 +1,5 @@
 // https://www.codewars.com/kata/5545f847bd94d050be00011d
+// find a word in a text string
 function wordSearch(word, text){
   const regex = new RegExp(`\\b${word}\\b`);
   return regex.test(text)
