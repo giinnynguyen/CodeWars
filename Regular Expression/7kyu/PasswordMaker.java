3f41708b7a2887efe3facecc88228cc3223a7789
giinnynguyen <tieuphung2205@gmail.com>
2022-11-03 10:50:51 +0800

    Create PasswordMaker.java

---
+++ Regular Expression/7kyu/PasswordMaker.java
@@ -0,0 +1,17 @@
+// https://www.codewars.com/kata/5637b03c6be7e01d99000046
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+public class PasswordMaker {
+   public static String makePassword(String phrase){
+      String password = "";
+        Pattern pattern = Pattern.compile("\\b\\w");
+        Matcher m = pattern.matcher(phrase);
+        while(m.find()) {
+            String firstChar = m.group();
+            firstChar = firstChar.replaceAll("o|O", "0").replaceAll("i|I", "1").replaceAll("s|S", "5");
+            password += firstChar;
+        }
+
+        return password;
+   }
+}
