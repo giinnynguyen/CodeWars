1c6850cc46ad1261109b4d7cd4fb82cc540e3ca6
giinnynguyen <tieuphung2205@gmail.com>
2022-11-17 17:44:45 +0800

    Update DisplayLargeNumber.js

--- Regular Expression/7kyu/DisplayLargeNumber.js
+++ Regular Expression/7kyu/DisplayLargeNumber.js
@@ -1,5 +1,6 @@
-// https://www.codewars.com/kata/57f318ce828b3553b40013ea (lookahead)
+// https://www.codewars.com/kata/57f318ce828b3553b40013ea 
 // format the large number. Exp: 123456789 to 123,456,789
+// lookahead
 function displayLargeNumber(n) {
   return n.toString().replace(/(\d)(?=(\d{3})+$)/g, "$1,")
 }
