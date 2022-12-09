abe35f942005d3d8d770f6262a588d31679503af
giinnynguyen <tieuphung2205@gmail.com>
2022-12-09 10:37:41 +0800

    Update RemoveConsecutiveDuplicate.java

--- Regular Expression/7kyu/RemoveConsecutiveDuplicate.java
+++ Regular Expression/7kyu/RemoveConsecutiveDuplicate.java
@@ -1,7 +1,7 @@
 // https://www.codewars.com/kata/5b39e91ee7a2c103300018b3
 // remove all consecutive duplicate words from a string, leaving only first words entries
 public class RemoveConsecutiveDuplicate {
-    // (?i): modifier turn on case-insensitive mode
+    // (?i): modifier to turn on case-insensitive mode
     // replacement string $1 means "group 1" (the first group made by a set of brackets). 
     public static String removeConsecutiveDuplicates(String s){
         return s.replaceAll("(?i)\\b(\\w+)(?:\\W+\\1\\b)+", "$1");
