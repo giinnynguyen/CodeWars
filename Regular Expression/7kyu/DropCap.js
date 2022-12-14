fc4e6dc2f1a8f67998a4deb0a49de5a4ab6b5407
giinnynguyen <tieuphung2205@gmail.com>
2022-12-14 13:55:54 +0800

    Update DropCap.js

--- Regular Expression/7kyu/DropCap.js
+++ Regular Expression/7kyu/DropCap.js
@@ -1,5 +1,6 @@
 // https://www.codewars.com/kata/559e5b717dd758a3eb00005a
-// match all the word have more than 2 character, then uppercase the first and lowercase the rest
+// requirement: capitalize every word that has length greater than 2, leaving smaller words as they are.
 function dropCap(n) {
+    // match all the word have more than 2 character, then uppercase the first and lowercase the rest
     return n.replace(/\b\w{3,}\b/g, (n) => n.charAt(0).toUpperCase() + n.slice(1).toLowerCase())
 }
